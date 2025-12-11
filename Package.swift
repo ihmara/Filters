ad90d0ba874062301ed2abad8fa305ed9d5b1969
Igor Khmara <igor.hmara@solbeg.com>
2025-12-11 12:58:38 +0300

    Use plugin

--- Package.swift
+++ Package.swift
@@ -12,12 +12,15 @@ let package = Package(
             targets: ["Filters"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/ihmara/iMetalPlugin.git", from: "1.0.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Filters"
+            name: "Filters",
+            plugins: ["iMetalPlugin"]
         ),
-
     ]
 )
